b92dd4e61c96a0d4d3ae5275530584b679d5b8cb
lucifer-code <56485755+lucifer-code@users.noreply.github.com>
2024-01-16 12:31:00 +0530

    Update database.js

--- backend/data/database.js
+++ backend/data/database.js
@@ -1,9 +1,9 @@
 import mongoose from "mongoose";
-const DB_URL = process.env.MONGO_URI || 'mongodb+srv://VishalAdmin:Vishal99_mon@cluster0.1og6vmz.mongodb.net/';
+const DB_URL = process.env.MONGO_URI || 'mongodb://localhost:27017';
 export const connectDB = () => {
   mongoose
-    // .connect("mongodb://localhost:27017", {
-    .connect("mongodb+srv://VishalAdmin:Vishal99_mon@cluster0.1og6vmz.mongodb.net/", {
+    .connect("mongodb://localhost:27017", {
+    
       dbName: "backendapi",
     })
     .then((c) => console.log(`Database Connected with ${c.connection.host}`))
